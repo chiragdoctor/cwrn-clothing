a98a66c59e36ca3b632a004094964716a7f18694
chiragdoctor <chirag.doctor@gmail.com>
2020-10-04 19:30:23 +0530

    updating application with reselect, repalced cart selectors witn memoized selectors

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
 
 import './CartIcon.scss';
 
@@ -14,8 +15,8 @@ const CartIcon = ({ count, toggleCartHidden }) => {
   );
 };
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-  count: cartItems.length,
+const mapStateToProps = state => ({
+  count: selectCartItemsCount(state),
 });
 
 const mapDispatchToProps = dispatch => ({
