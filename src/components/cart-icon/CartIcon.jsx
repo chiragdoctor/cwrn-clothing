d18f40813db6070dbee2b2c3cf98f019cdedb30c
chiragdoctor <chirag.doctor@gmail.com>
2020-10-04 19:02:15 +0530

    created cart-item component and connected our cart dropdown with cartItem

--- src/components/cart-icon/CartIcon.jsx
+++ src/components/cart-icon/CartIcon.jsx
@@ -5,17 +5,21 @@ import { toggleCartHidden } from '../../redux/cart/cart.actions';
 
 import './CartIcon.scss';
 
-const CartIcon = ({ toggleCartHidden }) => {
+const CartIcon = ({ count, toggleCartHidden }) => {
   return (
     <div className='cart-icon' onClick={toggleCartHidden}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>0</span>
+      <span className='item-count'>{count}</span>
     </div>
   );
 };
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  count: cartItems.length,
+});
 
 const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
